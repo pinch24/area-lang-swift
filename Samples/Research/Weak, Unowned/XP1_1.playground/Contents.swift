8f14f331e17fa2afc97e5135d4d9a64bf715bc4e
mk <pinch24@gmail.com>
2026-09-19 16:47:23 +0900

    2026.09.19

---
+++ Samples/Research/Weak, Unowned/XP1_1.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class A {
+	var b: Int = 1
+	
+	func afterTwoSecPrint() {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+			self.b = 99
+			print("b: \(self.b)")
+		}
+	}
+	
+	init() {
+		print("Memory Allocated.")
+	}
+	
+	deinit {
+		print("Memory Released.")
+	}
+}
+
+var a: A? = A()
+
+a?.afterTwoSecPrint()
+
+a = nil
+print("a Assigned to nil.")
